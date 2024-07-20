b0e052ff94b665190bf1273a6e0129e5b7d4281f
minamieri <sunaga.eri.mail@gmail.com>
2024-07-20 20:43:40 +0900

    修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,12 @@
 class CommentsController < ApplicationController 
-
-
   def create
-    @prototype = Prototype.find(params[:prototype_id])
-    @comment = @prototype.comments.build(comment_params)
-    @comment.user = current_user
+    @comment = Comment.new(comment_params)
     if @comment.save
-    redirect_to prototype_path(@comment.prototype)
+      redirect_to prototype_path(@comment.prototype) # 今回の実装には関係ありませんが、このようにPrefixでパスを指定することが望ましいです。
     else
-     @comments = @prototype.comments.includes(:user) 
-     render "prototypes/show" 
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
+      render "prototypes/show" # views/tweets/show.html.erbのファイルを参照しています。
     end
   end
 
